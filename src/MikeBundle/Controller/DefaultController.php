3320da26655e0dcacaedaf16afb6c442ed0e07fb
Pitz Michaël <michaeljpitz@gmail.com>
2017-09-14 10:44:25 +0200

    generate:bundle, mise dans autoload de composer.json, puis commande composer: composer dumpautoload, puis changement de route

---
+++ src/MikeBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MikeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('MikeBundle:Default:index.html.twig');
+    }
+}
---
+++ src/MikeBundle/MikeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MikeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MikeBundle extends Bundle
+{
+}
